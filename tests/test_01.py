14cd76ef005aa4639b4d338ca836f1c1bc7605f1
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-02-10 15:33:08 -0800

    Need to figure out a neat way to manage packages

--- tests/test_01.py
+++ tests/test_01.py
@@ -1,7 +1,10 @@
+########## Test common
 import tk3dv.common as tkc
-
 print(tkc.utilities.getCurrentEpochTime())
 
+########## Test nocstools
 import tk3dv.nocstools as nt
 
 print('nocstools verion:', nt.__version__)
+
+########## Test pyEasel
