b8aeae5fc30040e1a92779abed042cf526401ac3
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-02-09 18:29:27 -0800

    trying to get setup.py to work

--- __main__.py
+++
@@ -1,9 +0,0 @@
-# The 3D Vision Toolkit (3dvtk) is a toolkit consisting of a collection of tools for deep learning and 3D computer vision.
-# Written by Srinath Sridhar (http://srinathsridhar.com)
-
-# Released under the MIT License, see LICENSE.txt
-
-__version__='0.1'
-
-if __name__ == '__main__':
-    print('Using 3dvtk version: ', __version__)
