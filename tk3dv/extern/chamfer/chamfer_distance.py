11879be6e5a9f796e0b2a2b9c18d733b0c8a9e40
Srinath Sridhar <drsrinathsridhar@gmail.com>
2019-05-14 12:43:53 +0200

    added anomaly detection

--- tk3dv/extern/chamfer/chamfer_distance.py
+++ tk3dv/extern/chamfer/chamfer_distance.py
@@ -11,28 +11,29 @@ cd = load(name='cd', sources=[os.path.join(FileDirPath, 'chamfer_distance.cpp'),
 class ChamferDistanceFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, xyz1, xyz2):
-        batchsize, n, _ = xyz1.size()
-        _, m, _ = xyz2.size()
-        xyz1 = xyz1.contiguous()
-        xyz2 = xyz2.contiguous()
-        dist1 = torch.zeros(batchsize, n)
-        dist2 = torch.zeros(batchsize, m)
+        with torch.autograd.set_detect_anomaly(True):
+            batchsize, n, _ = xyz1.size()
+            _, m, _ = xyz2.size()
+            xyz1 = xyz1.contiguous()
+            xyz2 = xyz2.contiguous()
+            dist1 = torch.zeros(batchsize, n)
+            dist2 = torch.zeros(batchsize, m)
 
-        idx1 = torch.zeros(batchsize, n, dtype=torch.int)
-        idx2 = torch.zeros(batchsize, m, dtype=torch.int)
+            idx1 = torch.zeros(batchsize, n, dtype=torch.int)
+            idx2 = torch.zeros(batchsize, m, dtype=torch.int)
 
-        if not xyz1.is_cuda:
-            cd.forward(xyz1, xyz2, dist1, dist2, idx1, idx2)
-        else:
-            dist1 = dist1.cuda()
-            dist2 = dist2.cuda()
-            idx1 = idx1.cuda()
-            idx2 = idx2.cuda()
-            cd.forward_cuda(xyz1, xyz2, dist1, dist2, idx1, idx2)
+            if not xyz1.is_cuda:
+                cd.forward(xyz1, xyz2, dist1, dist2, idx1, idx2)
+            else:
+                dist1 = dist1.cuda()
+                dist2 = dist2.cuda()
+                idx1 = idx1.cuda()
+                idx2 = idx2.cuda()
+                cd.forward_cuda(xyz1, xyz2, dist1, dist2, idx1, idx2)
 
-        ctx.save_for_backward(xyz1, xyz2, idx1, idx2)
+            ctx.save_for_backward(xyz1, xyz2, idx1, idx2)
 
-        return dist1, dist2
+            return dist1, dist2
 
     @staticmethod
     def backward(ctx, graddist1, graddist2):
