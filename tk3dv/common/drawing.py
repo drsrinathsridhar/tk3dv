d6142da2ed0e4a6319d911a21ab99defc87c2d9f
Srinath Sridhar <drsrinathsridhar@gmail.com>
2021-07-27 20:42:37 -0400

    Update drawing.py

--- tk3dv/common/drawing.py
+++ tk3dv/common/drawing.py
@@ -1,10 +1,10 @@
 ## Fix for Mac OS Big Sur - Starts
 try:
-    import OpenGL as ogl
+    import OpenGL as OpenGL
     try:
         import OpenGL.GL   # this fails in <=2020 versions of Python on OS X 11.x
     except ImportError:
-        print('Drat, patching for Big Sur')
+        print('Drat, patching for Big Sur')  # https://stackoverflow.com/questions/63475461/unable-to-import-opengl-gl-in-python-on-macos
         from ctypes import util
         orig_util_find_library = util.find_library
         def new_util_find_library( name ):
@@ -15,9 +15,9 @@ try:
 except ImportError:
     pass
 ## Fix for Mac OS Big Sur - Ends
-import ogl.GL as gl
-import ogl.GLU as glu
-import ogl.arrays.vbo as glvbo
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
+import OpenGL.arrays.vbo as glvbo
 import numpy as np
 import math, sys
 
