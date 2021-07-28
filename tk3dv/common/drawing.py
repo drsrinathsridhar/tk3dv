712d416ae32be437267ae83bd184cf593d4d6c8b
Srinath Sridhar <drsrinathsridhar@gmail.com>
2021-07-27 20:40:44 -0400

    Kluge fix for Mac OS OpenGL bug

--- tk3dv/common/drawing.py
+++ tk3dv/common/drawing.py
@@ -1,6 +1,23 @@
-import OpenGL.GL as gl
-import OpenGL.GLU as glu
-import OpenGL.arrays.vbo as glvbo
+## Fix for Mac OS Big Sur - Starts
+try:
+    import OpenGL as ogl
+    try:
+        import OpenGL.GL   # this fails in <=2020 versions of Python on OS X 11.x
+    except ImportError:
+        print('Drat, patching for Big Sur')
+        from ctypes import util
+        orig_util_find_library = util.find_library
+        def new_util_find_library( name ):
+            res = orig_util_find_library( name )
+            if res: return res
+            return '/System/Library/Frameworks/'+name+'.framework/'+name
+        util.find_library = new_util_find_library
+except ImportError:
+    pass
+## Fix for Mac OS Big Sur - Ends
+import ogl.GL as gl
+import ogl.GLU as glu
+import ogl.arrays.vbo as glvbo
 import numpy as np
 import math, sys
 
